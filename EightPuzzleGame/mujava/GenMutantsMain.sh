a40b5eb52c7060816ca0ed6fd83bb8662b37f410
Rodrigo A. Costa <my@email.org>
2017-05-09 10:31:06 -0300

    correção path muja files

--- EightPuzzleGame/mujava/GenMutantsMain.sh
+++ EightPuzzleGame/mujava/GenMutantsMain.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export MJDIR=/media/12104064/6559-AC7F/workspace/EightPuzzleGame/mujava
+export MJDIR=/media/12104064/6559-AC7F/workspace/INE5448/EightPuzzleGame/mujava
 export CLASSPATH=$JAVA_HOME/lib/tools.jar:$MJDIR/mujava.jar:$MJDIR/openjava.jar
 echo $CLASSPATH 
 java mujava.gui.GenMutantsMain
