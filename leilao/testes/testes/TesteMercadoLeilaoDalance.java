8aff15125e7f27733a95d286c3202cbc65a957cb
Rodrigo A. Costa <rodrigoacosta02@hotmail.com>
2017-06-13 21:11:53 -0300

    refact testes scova

--- leilao/testes/testes/TesteMercadoLeilaoDalance.java
+++ leilao/testes/testes/TesteMercadoLeilaoDalance.java
@@ -10,102 +10,142 @@ import org.junit.Test;
 
 import modelo.Lance;
 import modelo.MercadoLeilao;
+import modelo.ProdutoLeilao;
+import modelo.Usuario;
 
 public class TesteMercadoLeilaoDalance {
 
+	private static final String NOME_COMPRADOR_1 = "User 1";
 	private final static String PRODUTO_1 = "Produto 1";
-	private final static String CPF_COMPRADOR = "01234567890";
+	private final static String CPF_COMPRADOR_1 = "01234567890";
 	private final static Double LANCE_MINIMO = 0.1;
 
 	private MercadoLeilao ml;
 
 	@Before
-	public void before() {
+	public void before() throws Exception {
 		this.ml = new MercadoLeilao();
-		this.cadastrarUsuarios("User 1", CPF_COMPRADOR);
+		this.cadastrarUsuarios(NOME_COMPRADOR_1, CPF_COMPRADOR_1);
 		this.cadastrarProdutosEmLeilao();
 	}
 
-	private void cadastrarProdutosEmLeilao() {
-		try {
-			this.ml.cadastrarProduto(PRODUTO_1, "produto descrição", LANCE_MINIMO, "01234567890",
-					new Date(new Date().getTime() + 10));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private void cadastrarProdutosEmLeilao() throws Exception {
+		this.ml.cadastrarProduto(PRODUTO_1, "produto descrição", LANCE_MINIMO, "01234567890",
+				new Date(new Date().getTime() + 10));
 	}
 
-	private void cadastrarUsuarios(String usuario, String cpf) {
-		try {
-			this.ml.cadastrarUsuario(usuario, "Rua das Flores", "a@a.com", cpf);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	private void cadastrarUsuarios(String usuario, String cpf) throws Exception {
+		this.ml.cadastrarUsuario(usuario, "Rua das Flores", "a@a.com", cpf);
+	}
+
+	/*
+	 * TESTES
+	 */
+
+	@Test(expected = Exception.class)
+	public void naoExisteProduto() throws Exception {
+		this.ml.daLance("A", CPF_COMPRADOR_1, 30.1);
 	}
 
 	@Test(expected = Exception.class)
-	public void testNaoExisteProduto() throws Exception {
-		this.ml.daLance("A", CPF_COMPRADOR, 30.1);
-	}
-
-	@Test(expected = Exception.class)
-	public void testNaoExisteComprador() throws Exception {
+	public void naoExisteComprador() throws Exception {
 		this.ml.daLance(PRODUTO_1, "", 0.0);
 	}
 
 	@Test(expected = Exception.class)
-	public void testValorInferior() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 0.0);
+	public void valorInferior() throws Exception {
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 0.0);
 	}
 
 	@Test(expected = Exception.class)
-	public void testDoisLancesSegundoInferiorAoPrimeiro() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.5);
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.0);
+	public void doisLancesSegundoInferiorAoPrimeiro() throws Exception {
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.5);
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.0);
 	}
 
 	@Test(expected = Exception.class)
-	public void testDoisLancesMesmoUsuarioIguais() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.0);
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.0);
+	public void doisLancesMesmoUsuarioIguais() throws Exception {
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.0);
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.0);
 	}
 
 	@Test
-	public void testDoisLancesMesmoUsuario() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.0);
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.5);
-		List<Lance> retornaLancesDeUmUsuario = this.ml.retornaLancesDeUmUsuario(CPF_COMPRADOR);
-		assertTrue(this.ml.getProdutosQueDeuLance(CPF_COMPRADOR).size() == 1);
-		assertTrue(this.ml.retornaLancesDeUmUsuario(CPF_COMPRADOR).size() == 2);
-		assertEquals(PRODUTO_1, retornaLancesDeUmUsuario.get(0).getNomeProdutoQueRecebeuOLance());
+	public void doisLancesMesmoUsuario() throws Exception {
+		Double valorLance_2 = 1.5;
+
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.0);
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, valorLance_2);
+
+		ProdutoLeilao produtoEmLeilao = (ProdutoLeilao) this.ml.getProdutosEmLeilao().get(0);
+		modelo.Lance lance_1 = this.ml.retornaTodosOsLancesEfetuados().get(0);
+		modelo.Lance lance_2 = this.ml.retornaTodosOsLancesEfetuados().get(1);
+
+		assertEquals(NOME_COMPRADOR_1, lance_1.getNomeDonoDoLance());
+		assertEquals(NOME_COMPRADOR_1, lance_2.getNomeDonoDoLance());
+		assertEquals(PRODUTO_1, lance_1.getNomeProdutoQueRecebeuOLance());
+		assertEquals(PRODUTO_1, lance_2.getNomeProdutoQueRecebeuOLance());
+
+		assertEquals(valorLance_2, produtoEmLeilao.retornaTodosOsLancesFeitosNesseProduto().get(1).getValorDoLance());
 	}
 
 	@Test
-	public void testDoisLancesDoisUsuario() throws Exception {
-		String user2 = "User 2";
-		String cpf2 = "09876543210";
-		this.cadastrarUsuarios(user2, cpf2);
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, 1.0);
-		this.ml.daLance(PRODUTO_1, cpf2, 1.5);
+	public void doisLancesDoisUsuario() throws Exception {
+		Double valorLance_2 = 1.5;
+		String nomeComprador_2 = "User 2";
+		String cpfComprador_2 = "09876543210";
 
-		assertTrue(this.ml.getProdutosQueDeuLance(CPF_COMPRADOR).size() == 1);
-		assertTrue(this.ml.getProdutosQueDeuLance(CPF_COMPRADOR).size() == 1);
-		assertTrue(this.ml.retornaLancesDeUmUsuario(cpf2).size() == 1);
-		assertTrue(this.ml.retornaLancesDeUmUsuario(cpf2).size() == 1);
+		this.cadastrarUsuarios(nomeComprador_2, cpfComprador_2);
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, 1.0);
+		this.ml.daLance(PRODUTO_1, cpfComprador_2, valorLance_2);
+
+		ProdutoLeilao produtoEmLeilao = (ProdutoLeilao) this.ml.getProdutosEmLeilao().get(0);
+		List<Lance> retornaLances = this.ml.retornaTodosOsLancesEfetuados();
+		Usuario user_1 = (Usuario) this.ml.getUsuariosCadastrados().get(0);
+		Usuario user_2 = (Usuario) this.ml.getUsuariosCadastrados().get(1);
+
+		assertEquals(user_1.getNome(), retornaLances.get(1).getNomeDonoDoLance());
+		assertEquals(user_1.getCpf(), retornaLances.get(1).getCpfDonoDoLance());
+		assertEquals("a@a.com", user_1.getEmail());
+		assertEquals("Rua das Flores", user_1.getEndereco());
+		assertEquals(0, user_1.getBensComprados().size());
+		assertEquals(0, user_1.getBensOfertados().size());
+
+		assertEquals(user_2.getNome(), retornaLances.get(0).getNomeDonoDoLance());
+		assertEquals(user_2.getCpf(), retornaLances.get(0).getCpfDonoDoLance());
+		assertEquals("a@a.com", user_1.getEmail());
+		assertEquals("Rua das Flores", user_2.getEndereco());
+		assertEquals(0, user_2.getBensComprados().size());
+		assertEquals(1, user_2.getBensOfertados().size());
+
+		assertEquals(PRODUTO_1, retornaLances.get(0).getNomeProdutoQueRecebeuOLance());
+		assertEquals(valorLance_2, produtoEmLeilao.retornaTodosOsLancesFeitosNesseProduto().get(1).getValorDoLance());
+		assertEquals(PRODUTO_1, retornaLances.get(0).getNomeProdutoQueRecebeuOLance());
+
 	}
 
 	@Test
-	public void testLance() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, Double.MAX_VALUE);
-		assertTrue(this.ml.getProdutosQueDeuLance(CPF_COMPRADOR).size() == 1);
-		assertTrue(this.ml.retornaLancesDeUmUsuario(CPF_COMPRADOR).size() == 1);
+	public void lance() throws Exception {
+		Double maxValue = Double.MAX_VALUE;
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, maxValue);
+		modelo.Lance lance = this.ml.retornaTodosOsLancesEfetuados().get(0);
+		ProdutoLeilao produtoEmLeilao = (ProdutoLeilao) this.ml.getProdutosEmLeilao().get(0);
+
+		assertEquals(PRODUTO_1, lance.getNomeProdutoQueRecebeuOLance());
+		assertEquals(NOME_COMPRADOR_1, lance.getNomeDonoDoLance());
+		assertEquals(maxValue, produtoEmLeilao.retornaTodosOsLancesFeitosNesseProduto().get(0).getValorDoLance());
+
 	}
 
 	@Test
-	public void testLanceMinimo() throws Exception {
-		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR, LANCE_MINIMO);
-		assertTrue(this.ml.getProdutosQueDeuLance(CPF_COMPRADOR).size() == 1);
-		assertTrue(this.ml.retornaLancesDeUmUsuario(CPF_COMPRADOR).size() == 1);
+	public void lanceMinimo() throws Exception {
+		this.ml.daLance(PRODUTO_1, CPF_COMPRADOR_1, LANCE_MINIMO);
+		modelo.Lance lance = this.ml.retornaTodosOsLancesEfetuados().get(0);
+		ProdutoLeilao produtoEmLeilao = (ProdutoLeilao) this.ml.getProdutosEmLeilao().get(0);
+
+		assertEquals(PRODUTO_1, lance.getNomeProdutoQueRecebeuOLance());
+		assertEquals(NOME_COMPRADOR_1, lance.getNomeDonoDoLance());
+		assertEquals(LANCE_MINIMO, produtoEmLeilao.retornaTodosOsLancesFeitosNesseProduto().get(0).getValorDoLance());
+
 	}
 
 }
