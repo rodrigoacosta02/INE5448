60ff2b0cce796840855679838f807319669689ef
Rodrigo A. Costa <rodrigoacosta02@hotmail.com>
2017-06-19 20:45:27 -0300

    fitnesse demais exercicios

--- INE5448-Leilao/testes/fixtures/ProdutoColumnFixtureTest.java
+++ INE5448-Leilao/testes/fixtures/ProdutoColumnFixtureTest.java
@@ -1,15 +1,16 @@
 package fixtures;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import fit.ColumnFixture;
 import modelo.FachadaMercadoLeilaoComSerializacao;
+import modelo.Produto;
 
 public class ProdutoColumnFixtureTest extends ColumnFixture {
-	public String nome;
-	public String usuarioAssociado;
-	String descricao;
-	private Date dataLimite;
+	private String nome;
+	private String descricao;
+	private String dataLimite;
 	private String cpfLeiloador;
 	private Double lanceMinimo;
 
@@ -21,10 +22,26 @@ public class ProdutoColumnFixtureTest extends ColumnFixture {
 
 	public boolean cadastrarProduto() {
 		try {
-			this.fachada.cadastrarProduto(this.nome, this.descricao, this.lanceMinimo, this.cpfLeiloador, this.dataLimite);
+			this.fachada.cadastrarProduto(this.nome, this.descricao, this.lanceMinimo, this.cpfLeiloador,
+					new SimpleDateFormat("yyyy-MM-dd").parse(this.dataLimite));
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public boolean verificarProdutoExiste() {
+		try {
+			List<Produto> produtosEmLeilao = (List<Produto>) this.fachada.getProdutosEmLeilao();
+			for (Produto produto : produtosEmLeilao) {
+				if (produto.nome().equals(this.nome)) {
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
--- INE5448-Leilao/testes/fixtures/ValidacaoDeLances.java
+++ INE5448-Leilao/testes/fixtures/ValidacaoDeLances.java
@@ -1,5 +1,25 @@
 package fixtures;
 
-public class ValidacaoDeLances {
+import fit.ColumnFixture;
+import modelo.FachadaMercadoLeilaoComSerializacao;
 
+public class ValidacaoDeLances extends ColumnFixture {
+	private String nomeProduto;
+	private String cpfComprador;
+	private Double valorLance;
+
+	FachadaMercadoLeilaoComSerializacao fachada = new FachadaMercadoLeilaoComSerializacao();
+
+	public ValidacaoDeLances() {
+		super();
+	}
+
+	public boolean realizarLance() {
+		try {
+			this.fachada.daLance(this.nomeProduto, this.cpfComprador, this.valorLance);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
